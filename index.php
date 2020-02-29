e89dda306b732d687bef4cff58cf1b69c352413e
leo <leocv0205@gmail.com>
2020-02-28 19:29:50 -0500

    se codifica arreglos indexados y asociativos

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Clase 4 bucles, arreglos y funciones<br>";
+//array o arreglo indexado
+$arreglo=array("Juan","Maria","Mames","Sandra");
+print_r($arreglo);
+echo("<br>......................................................................<br>");
+
+//array asociativo
+$arreglo=array('usuario1'=>"Juan",'usuario2'=>"Maria",'usuario3'=>"James",'usuario4'=>"Sandra");
+print_r($arreglo);
+
+
+
+
+?>
